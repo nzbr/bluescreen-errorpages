70cd9ba30baff88a739aeb01fa29b87fcaebdabc
nzbr <mail@nzbr.de>
2020-09-08 10:13:01 +0200

    Remove splash from Ctrl+Alt+Delete; Make domain name changeable via build.py; Add auto-reload after 20s (Fixes #1)

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 Error pages for a webserver that look like a Windows 9x BSOD  
 They are developed for and currently in use at [https://nzbr.de](https://nzbr.de)
 
-`./build.py` may be used to create the actual html-files for the webserver
-
-If you consider using these, you may want to remove the `nzbr.de` branding from just about everywhere
+`./build.py <domain>` may be used to create the actual html-files for the webserver
--- build.py
+++ build.py
@@ -1,8 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import yaml
 import os
+from sys import argv
 from jinja2 import Template
+
+if len(argv) < 2:
+    print("You have to specify a domain name")
+    exit(1)
+
+domain = argv[1]
 
 with open("errors.yml", "r") as f:
     errors = next(yaml.load_all(f, Loader=yaml.SafeLoader))
@@ -16,6 +23,7 @@ if not os.path.isdir("build"):
     os.mkdir("build")
 
 for err in errors:
+    err["domain"] = domain
     html = tpl.render(err)
     with open("build/"+str(err["err"])+".html", "w") as f:
         f.write(html)
