3c3201022a79b081a1e0421db6d19c2fa1a64c25
nzbr <mail@nzbr.de>
2022-05-26 20:38:58 +0200

    embed font into html

--- build.py
+++ build.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-import yaml
+import base64
 import os
+import tempfile
 from sys import argv
+from urllib.request import urlopen
+
+import yaml
+from fontTools.ttLib import TTFont
 from jinja2 import Template
 
 if len(argv) < 2:
@@ -10,6 +15,22 @@ if len(argv) < 2:
     exit(1)
 
 domain = argv[1]
+
+# Download the DOS font
+fontSrc = "http://laemeur.sdf.org/fonts/MorePerfectDOSVGA.ttf"
+data = urlopen(fontSrc).read()
+# Convert it to WOFF
+woffdata = None
+with tempfile.SpooledTemporaryFile() as i:
+  with tempfile.SpooledTemporaryFile() as o:
+    i.write(data)
+    with TTFont(i) as ttFont:
+      ttFont.flavor = "woff2"
+      ttFont.save(o)
+    o.seek(0)
+    woffdata = o.read()
+# To Base64
+font = "".join(base64.encodebytes(woffdata).decode("UTF-8").split("\n"))
 
 with open("errors.yml", "r") as f:
     errors = next(yaml.load_all(f, Loader=yaml.SafeLoader))
@@ -24,6 +45,7 @@ if not os.path.isdir("build"):
 
 for err in errors:
     err["domain"] = domain
+    err["font"] = font
     html = tpl.render(err)
     with open("build/"+str(err["err"])+".html", "w") as f:
         f.write(html)
